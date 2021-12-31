17657edcc8403a9c336e71c804efddc86a92e2de
ashpak.farish@awanoo.com <ashpak.farish@barunitsolutions.com>
2022-01-01 01:57:17 +0530

    Starting Phase

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './Components/NavBar.js/NavBar';
+// import Home from './Pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar />
+    </div>
+  );
+}
+
+export default App;
