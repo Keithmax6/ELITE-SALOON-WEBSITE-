89067b6b070716074ee1014cf44e05f8ff611545
ashpak.farish@awanoo.com <ashpak.farish@barunitsolutions.com>
2022-01-10 20:13:12 +0530

    services to be done

--- src/Pages/ServicePage/Service.js
+++ src/Pages/ServicePage/Service.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Service.css';
-import WebFont from 'webfontloader';
+import barber from '../../assets/barber.jpeg';
+import shaving from '../../assets/shaving.jpeg';
+import styling from '../../assets/styling.jpeg';
 
 
 const Service = () => {
     return (
         <React.Fragment>
-            <div className='serviceheader'>
-                <h1>OUR SERVICES</h1>
-            </div>
+            <h1 className='serviceheader'> Our Services</h1>
+        <div className='section1'>
+           
+        </div>
         </React.Fragment>
     )
 }
+
+{/* <img src={styling} alt="" className='image3'/>    
+        <img src={styling} alt="" className='image4'/>     */}
 
 export default Service
